86eacb4ff287b2690ed1b24abe3f63bc7c6be59a
speedstarkiwi <121459772+speedstarkiwi@users.noreply.github.com>
2023-04-15 20:08:58 -0400

    Update remade.cpp

--- remade.cpp
+++ remade.cpp
@@ -1,12 +1,19 @@
 namespace luau {
-    inline void lua_pushnil(std::uintptr_t rL) {
+    //extreme messy bcuz no time i did this while studying for final exams so mb skid
+    void setobj69(uintptr_t L, void* obj1, const void* obj2) {
+        const void* o2 = obj2;
+        void* o1 = obj1;
+        memcpy(o1, o2, sizeof(void*));
+    }
+
+    inline void lua_pushnil(std::uintptr_t rL) {//only push nil or nothing lmao
         const std::uintptr_t top_ptr = rL + top;
         const std::uintptr_t topp = *reinterpret_cast<std::uintptr_t*>(top_ptr);
         *reinterpret_cast<std::uintptr_t*>(topp) = LUA_TNIL;
         *reinterpret_cast<std::uintptr_t*>(top_ptr) += sizeof(std::uintptr_t);
     }
 
-    inline std::uintptr_t lua_gettop(std::uintptr_t rL) {
+    inline std::uintptr_t lua_gettop(std::uintptr_t rL) {//used for arguments stupid skid
         const std::uintptr_t top_ptr = rL + top;
         const std::uintptr_t base_ptr = rL + base;
         const std::uintptr_t top_val = *reinterpret_cast<std::uintptr_t*>(top_ptr);
@@ -14,7 +21,7 @@ namespace luau {
         return (top_val - base_val) >> 4;
     }
 
-    inline void lua_settop(std::uintptr_t rL, std::int32_t idx) {
+    inline void lua_settop(std::uintptr_t rL, int idx) {//tells us to set the top
         const std::uintptr_t top_ptr = rL + top;
         const std::uintptr_t base_ptr = rL + base;
         std::uintptr_t top_val = *reinterpret_cast<std::uintptr_t*>(top_ptr);
@@ -33,27 +40,44 @@ namespace luau {
         }
     }
 
-    std::uintptr_t lua_index2addr(uintptr_t rL, std::int32_t idx)
+    std::uintptr_t lua_index2addr(uintptr_t rL, std::int32_t idx)//index2 addr or cases to pseudo2 if idx is not matched correct
     {
         std::uintptr_t basee = rL + top;
         std::uintptr_t topp = rL + base;
 
-        if (idx > 0)
+        if (idx > 0)//if its greater then NULL lool
         {
             std::uintptr_t o = (*reinterpret_cast<std::uintptr_t*>(basee) + (idx - 1) * sizeof(std::uintptr_t));
             if (o >= *reinterpret_cast<std::uintptr_t*>(top))
-                return reinterpret_cast<std::uintptr_t>(nullptr);
+                return (nilobject_address);
             else
                 return o;
         }
-        else if (idx > -10000)
+        else if (idx > LUA_REGISTRYINDEX)//-10000 negative
         {
-            return *reinterpret_cast<std::uintptr_t*>(top) + idx * sizeof(std::uintptr_t);
+            return *reinterpret_cast<std::uintptr_t*>(top) + idx * sizeof(std::uintptr_t);//return this
         }
-        else
+        else//none of the index match -> go to pseudo2 and let it handle the rest of the cases
         {
-            return lua_pseudo2addr(rL, idx);
+           // return lua_pseudo2addr(rL, idx);
         }
     }
 
+    const uintptr_t luaA_toobject(uintptr_t rL, int idx)//technically converts a  stack index to a ptr
+    {
+        const uintptr_t p = lua_index2addr(rL, idx);
+        return (p == nilobject_address) ? NULL : p;
+    }
+
+    uintptr_t lua_newthread(uintptr_t rL) {//no i didnt make cuz skid moment but it works tho :troll:
+        return rL;
+    }
+
+    void lua_pushnumber(uintptr_t rL, double n)//spent 69 hours making it
+    {
+        std::uintptr_t i_o = reinterpret_cast<std::uintptr_t>(rL + top); 
+        *(lua_Number*)(i_o) = (n); //is a lua numbar
+
+        *reinterpret_cast<std::uintptr_t*>(rL + top) += sizeof(TValue);
+    }
 }
